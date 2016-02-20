f0ac2db48fe6e7d81761ebb3fbbc9493d978ffca
jcontour <contj186@newschool.edu>
2016-02-19 23:23:44 -0500

    uerghhh

--- background/analytics.js
+++ background/analytics.js
@@ -7,10 +7,6 @@
 // 	console.log(tab[0].url);
 // })
 
-// chrome.tabs.onActivated.addListener(function(info) {
-// 	console.log("switched");
-// });
-
 chrome.tabs.onActivated.addListener(function(info) {
 	chrome.tabs.query({active: true}, function(tab){
 		var url = tab[0].url;
