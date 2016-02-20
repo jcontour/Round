88499fce30292ed0a0650fae6e1905573c5fa4c2
jcontour <contj186@newschool.edu>
2016-02-19 23:14:24 -0500

    blergh
    i dunno

---
+++ background/analytics.js
@@ -0,0 +1,19 @@
+// chrome.tabs.getSelected(null,function(tab) {
+//     var url = tab.url;
+//     console.log(url);
+// });
+
+// chrome.tabs.query({active: true}, function(tab){
+// 	console.log(tab[0].url);
+// })
+
+// chrome.tabs.onActivated.addListener(function(info) {
+// 	console.log("switched");
+// });
+
+chrome.tabs.onActivated.addListener(function(info) {
+	chrome.tabs.query({active: true}, function(tab){
+		var url = tab[0].url;
+		console.log(url);
+	})
+});
