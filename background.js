54ff63c0005fe5e7d2af563e1fd03a6b39a071b5
jcontour <contj186@newschool.edu>
2016-02-26 23:11:20 -0500

    communication working

---
+++ background.js
@@ -0,0 +1,39 @@
+var value = 0;
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        switch (request.directive) {
+        case "article-click":
+            var url;
+
+            chrome.tabs.query({active: true}, function(tab){
+                    url = tab[0].url;
+                    console.log(url);
+            })
+
+            // execute the content script
+            chrome.tabs.executeScript(null, { // defaults to the current tab
+                file: "contentscript.js", // script to inject into page and run in sandbox
+                // allFrames: true // This injects script into iframes in the page and doesn't work before 4.0.266.0.
+            });
+
+            console.log("article button clicked");
+            value ++;
+            sendResponse({value: value}); // sending back response
+            break;
+
+        case "profile-click":
+            // execute the content script
+            chrome.tabs.executeScript(null, { // defaults to the current tab
+                file: "contentscript.js", // script to inject into page and run in sandbox
+                // allFrames: true // This injects script into iframes in the page and doesn't work before 4.0.266.0.
+            });
+            console.log("profile button clicked");
+            sendResponse({data: "this is profile data!!"}); // sending back response to sender
+            break;
+        default:
+            // helps debug when request directive doesn't match
+            alert("Unmatched request of '" + request + "' from script to background.js from " + sender);
+        }
+    }
+);
--- pages/js/popup.js
+++ pages/js/popup.js
@@ -1,5 +1,17 @@
-$('#button').click(function(){
-	// var newURL = "http://www.google.com";
- //    chrome.tabs.create({ url: newURL });
- console.log("hello");
+function articleHandler(e) {
+    chrome.runtime.sendMessage({directive: "article-click"}, function(response) {
+    	$("body").append("<p>" + response.value + "</p>");
+        // this.close(); // close the popup when the background finishes processing request
+    });
+}
+
+function profileHandler(e) {
+    chrome.runtime.sendMessage({directive: "profile-click"}, function(response) {
+        // this.close(); // close the popup when the background finishes processing request
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('article').addEventListener('click', articleHandler);
+    document.getElementById('profile').addEventListener('click', profileHandler);
 })
